eae1fdfbbb4232473c4ebca6c55fc247a8a4adde
Takayuki0924 <takapon0924@icloud.com>
2023-10-06 09:39:55 +0900

    2023-10-06 Update backend

---
+++ backend/routers/calculate.ts
@@ -0,0 +1,66 @@
+import { Router, Request, Response } from "express";
+
+//type
+type valuesType = {
+    value1: string;
+    value2: string;
+};
+
+//routerの設定
+export const calculateRouter: Router = Router();
+
+//足し算
+calculateRouter.post("/plus", (req: Request, res: Response) => {
+    const values: valuesType = req.body;
+
+    try {
+        const numResult: number = Number(values.value1) + Number(values.value2);
+        const result = String(numResult);
+        return res.status(200).json(result); 
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: "計算に失敗しました" });
+    };
+});
+
+//引き算
+calculateRouter.post("/minus", (req: Request, res: Response) => {
+    const values: valuesType = req.body;
+
+    try {
+        const numResult: number = Number(values.value1) - Number(values.value2);
+        const result = String(numResult);
+        return res.status(200).json(result); 
+    } catch(err) {
+        console.error(err);
+        return res.status(500).json({ error: "計算に失敗しました" });
+    };
+});
+
+//かけ算
+calculateRouter.post("/multiplication", (req: Request, res: Response) => {
+    const values: valuesType = req.body;
+
+    try {
+        const numResult: number = Number(values.value1) * Number(values.value2);
+        const result = String(numResult);
+        return res.status(200).json(result); 
+    } catch(err) {
+        console.error(err);
+        return res.status(500).json({ error: "計算に失敗しました" });
+    };
+});
+
+//割り算
+calculateRouter.post("/division", (req: Request, res: Response) => {
+    const values: valuesType = req.body;
+
+    try {
+        const numResult: number = Math.floor((Number(values.value1) / Number(values.value2)) * 100) / 100;
+        const result = String(numResult);
+        return res.status(200).json(result); 
+    } catch(err) {
+        console.error(err);
+        return res.status(500).json({ error: "計算に失敗しました" });
+    };
+});
--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,16 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
+import cors from "cors";
+
+//routers
+import { calculateRouter } from "./routers/calculate";
+
 
 const app: Application = express();
 const PORT = 8080;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1", calculateRouter);
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
