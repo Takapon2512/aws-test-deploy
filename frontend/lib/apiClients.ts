c009ace3f47825a73d04d97ad997f19f58219ca2
Takayuki0924 <takapon0924@icloud.com>
2023-11-04 10:39:08 +0900

    Update frontend & backend

--- frontend/lib/apiClients.ts
+++ frontend/lib/apiClients.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-    baseURL: "http://10.0.1.40:80/api/v1",
+    baseURL: "http://localhost:8080/api/v1",
     headers: {
         "Content-Type": "application/json"
     }
